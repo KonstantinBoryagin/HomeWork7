fa651f0c39dd0c8750f3f7d1dbbbce3d5f45eb57
Konstantin Boryagin <nokss89@gmail.com>
2021-08-26 16:44:55 +0300

    add .gitignore and slf4j to maven dependecies

---
+++ src/main/java/ru/otus/hw7_exceptions/Main.java
@@ -0,0 +1,16 @@
+package ru.otus.hw7_exceptions;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        //System.out.println(Main.class);
+        //logger.info("start");
+
+
+    }
+}
